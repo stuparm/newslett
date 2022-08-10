0df2f91f8260d871e3bf271ca4b9f3ec79e1e138
Mihailo Stupar <stupar.mih@gmail.com>
2022-08-10 13:13:10 +0200

    remove coments from start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@ echo 'initializing bridge docker network <newslettnetwork> ...'
 docker network create newslettnetwork
 
 
-
 echo 'starting the mongo database (with replica set enabled) ...'
 docker run --rm -d -p 27017:27017 -h $(hostname) \
    	--add-host=host.docker.internal:host-gateway \
@@ -34,44 +33,3 @@ echo ''
 echo ''
 echo 'tailing the logs of <contentinquiry> app. Logs will contains query terms and found rss feed ...'
 docker logs -f contentinquiry
-
-
-
-
-
-
-
-# docker run --rm -d -p 27017:27017 -h $(hostname) --add-host=host.docker.internal:host-gateway --network newslettnetwork --name mongo mongo:4.4.3 --replSet=rs0 && sleep 4 && docker exec mongo mongo --eval "rs.initiate();"
-# docker exec -it mongo /bin/bash 
-# mongo use newslett
-# use newslett
-# db.createCollection( 'feed' );
-
-
-
-# #       db.createCollection('r')
-
-# #       db.createCollection( 'feed' );
-# #       db.feed.createIndex( {  "guid": 1 }, {  unique: true } );
-
-
-
-
-# docker run --rm -d -p 27017:27017 -h $(hostname) \
-#    	--add-host=host.docker.internal:host-gateway \
-#    	--network newslettnetwork \
-#    	--name mongo \
-#    	--volume ${PWD}/mongo-setup.js:/etc/mongo/mongo-setup.js \
-#    	mongo:4.4.3 --replSet=rs0 && \
-#    	sleep 4 && \
-#    	docker exec mongo mongo --eval "rs.initiate();"
-#    	docker exec mongo mongo --eval "load('/etc/mongo/mongo-setup.js')"
-
-
-# # docker exec mongo mongo --eval "rs.initiate();" \
-
-
-# # docker run --rm -d -p 27017:27017 -h $(hostname) --add-host=host.docker.internal:host-gateway --network newslettnetwork --name mongo mongo:4.4.3 --replSet=rs0 && sleep 4 && docker exec mongo mongo --eval "rs.initiate();"
-
-
-# # docker exec mongo mongo --eval "use newslett"
